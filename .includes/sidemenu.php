76d5ffc6d7dad24ec2e1fa95b7745dbd1bcd5a85
thalia-sarah <ritasaparina1983@gmail.com>
2025-02-15 17:06:02 +0700

    edit sidemenu

--- .includes/sidemenu.php
+++ .includes/sidemenu.php
@@ -1,8 +1,8 @@
 <!-- Menu -->
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
   <div class="app-brand demo">
-    <a href="./dashboard.php" class="app-brand-link">
-      <span class="app-brand-text demo menu-text fw-bolder ms-2 text-uppercase">BusReservation</span>
+    <a href="./beranda.php" class="app-brand-link">
+      <span class="app-brand-text demo menu-text fw-bolder ms-2 text-uppercase">Buss</span>
     </a>
     <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
       <i class="bx bx-chevron-left bx-sm align-middle"></i>
@@ -12,28 +12,28 @@
   <ul class="menu-inner py-1">
     <!-- Dashboard -->
     <li class="menu-item">
-      <a href="dashboard.php" class="menu-link">
+      <a href="beranda.php" class="menu-link">
         <i class="menu-icon tf-icons bx bx-home-circle"></i>
-        <div data-i18n="Analytics">Dashboard</div>
+        <div data-i18n="Analytics">Beranda</div>
       </a>
     </li>
     <!-- Forms & Tables -->
-    <li class="menu-header small text-uppercase"><span class="menu-header-text">Posts</span></li>
+    <li class="menu-header small text-uppercase"><span class="menu-header-text">Pesanan</span></li>
     <!-- Forms -->
     <li class="menu-item">
       <a href="javascript:void(0);" class="menu-link menu-toggle">
         <i class="menu-icon tf-icons bx bx-detail"></i>
-        <div data-i18n="Posts">Posts</div>
+        <div data-i18n="Pesanan">Pesanan</div>
       </a>
       <ul class="menu-sub">
         <li class="menu-item">
-          <a href="posts.php" class="menu-link">
-            <div data-i18n="Basic Inputs">Post Baru</div>
+          <a href="pesanan.php" class="menu-link">
+            <div data-i18n="Basic Inputs">Buat Pesanan</div>
           </a>
         </li>
         <li class="menu-item">
-          <a href="kategori.php" class="menu-link">
-            <div data-i18n="Input groups">Kategori</div>
+          <a href="rute.php" class="menu-link">
+            <div data-i18n="Input groups">Rute</div>
           </a>
         </li>
       </ul>
